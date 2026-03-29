ef67a086a2a712fa882ba01d952ab039c65e2e26
Ryloe <ryan.loeffler@gmx.com>
2026-03-29 12:07:11 -0400

    Update Categories

--- backend/RestaurantAnalysis/FullAPIPull.py
+++ backend/RestaurantAnalysis/FullAPIPull.py
@@ -12,14 +12,18 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 SERPAPI_KEY = os.getenv("SERPAPI_KEY")
 
 CATEGORIES = [
-    "acai_shop", "american_restaurant", "asian_fusion_restaurant", "asian_restaurant",
-    "bagel_shop", "bakery", "bar", "barbecue_restaurant", "bistro", "breakfast_restaurant",
-    "brewery", "british_restaurant", "brunch_restaurant", "burger_restaurant", "cafe",
-    "chicken_wings_restaurant", "chinese_restaurant", "coffee_shop", "cocktail_bar",
-    "dessert_shop", "diner", "french_restaurant", "greek_restaurant", "indian_restaurant",
-    "italian_restaurant", "japanese_restaurant", "mexican_restaurant", "pizza_restaurant",
-    "seafood_restaurant", "steak_house", "sushi_restaurant", "thai_restaurant",
-    "vegan_restaurant", "vietnamese_restaurant", "wine_bar"
+    "acai_shop", "afghani_restaurant", "african_restaurant", "american_restaurant", "asian_restaurant",
+    "bagel_shop", "bakery", "bar", "bar_and_grill", "barbecue_restaurant", "brazilian_restaurant", "breakfast_restaurant",
+    "brunch_restaurant", "buffet_restaurant", "cafe", "cafeteria", "candy_store", "cat_cafe", "chinese_restaurant",
+    "chocolate_factory", "chocolate_shop", "coffee_shop", "confectionery", "deli", "dessert_restaurant",
+    "dessert_shop", "diner", "dog_cafe", "donut_shop", "fast_food_restaurant", "fine_dining_restaurant",
+    "food_court", "french_restaurant", "greek_restaurant", "hamburger_restaurant", "ice_cream_shop",
+    "indian_restaurant", "indonesian_restaurant", "italian_restaurant", "japanese_restaurant",
+    "juice_shop", "korean_restaurant", "lebanese_restaurant", "meal_delivery", "meal_takeaway",
+    "mediterranean_restaurant", "mexican_restaurant", "middle_eastern_restaurant", "pizza_restaurant",
+    "pub", "ramen_restaurant", "restaurant", "sandwich_shop", "seafood_restaurant", "spanish_restaurant",
+    "steak_house", "sushi_restaurant", "tea_house", "thai_restaurant", "turkish_restaurant",
+    "vegan_restaurant", "vegetarian_restaurant", "vietnamese_restaurant", "wine_bar"
 ]
 
 WAIT_KEYWORDS = ["wait", "line", "packed", "busy", "crowded", "hour"]

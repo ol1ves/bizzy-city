295a8c115b2e3a430063bda91485f8c8b84a6c66
ol1ves <103908970+ol1ves@users.noreply.github.com>
2026-03-30 13:07:31 -0400

    Add analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased">
       <body className={`${jakarta.className} h-full`}>
         <MapProvider>{children}</MapProvider>
+        <Analytics />
       </body>
     </html>
   );
